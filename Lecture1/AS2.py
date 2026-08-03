04ed61b465164937768edcefdb2ed7897dd4c28d
Bruunemma81 <bruunemma81@gmail.com>
2026-08-03 14:34:26 +0200

    Fixed comments not being pushed

--- Lecture1/AS2.py
+++ Lecture1/AS2.py
@@ -1,16 +1,26 @@
 import pandas as pd
+import os
 
-df = pd.read_csv("auto.csv")
+# Read the CSV file using the path relative to this Python file
+df = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + "/csv/auto.csv")
 
+# Remove all rows where 'mpg' is lower than 16
 df = df[df["mpg"] >= 16]
 
+# Display the first 7 rows of the 'weight' and 'acceleration' columns
 print(df[["weight", "acceleration"]].head(7))
 
+# Remove rows where 'horsepower' contains '?'
 df = df[df["horsepower"] != "?"]
+
+# Converts the 'horsepower' column from string to integer
 df["horsepower"] = df["horsepower"].astype(int)
 
+# Calculates the average of all columns except 'name'
 average = df.drop(columns=["name"]).mean()
 
+# Prints averages
 print(average)
 
+# Displays the first 5 rows of the filtered Data
 print(df.head())
